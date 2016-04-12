e54ef1df270b445056e219ea70e3a9192e32b67a
GuranCarmen <georgyana_severin_06@yahoo.com>
2016-04-12 23:06:31 +0300

    Create producer.java

---
+++ TD2/producer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package td2;
+
+/**
+ *
+ * @author Carmen
+ */
+public class Producer extends Thread {
+    
+    @Override
+    public void run(){
+        
+    }
+}
